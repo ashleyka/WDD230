121b3c8fbb0676fa235cc7eaf1723c1e3bfe0308
ashleyka <ashleykamujoma@gmail.com>
2023-10-12 19:05:13 +0200

    wdd230

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -15,7 +15,38 @@ const currentYearOptions = {
 lastModified.textContent = ` Last Modified: ${new Date(document.lastModified).toLocaleDateString("en-US", lastModifiedOptions)}`;
 currentYear.textContent = `${new Date().toLocaleDateString("en-US", currentYearOptions)}`
 
+document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
 
-const icon = document.querySelector('icon');
 
-icon.addEventListener('click',()=>{mainTag.classList.toggle('dark-theme')});
+
+// dark mode stuff here
+const modeBtn = document.querySelector('#mode');
+const theMainArea = document.querySelector('main')
+modeBtn.addEventListener('click', () => {
+  console.log('clicked')
+  modeBtn.classList.toggle('dark')
+  theMainArea.classList.toggle('dark')
+})
+
+
+//Current Weather
+
+const myAPI = "437209d363a4387bb2f5dffcea58bbee"
+const myWeather = `//api.openweathermap.org/data/2.5/weather?zip=84653,us&appid=437209d363a4387bb2f5dffcea58bbee&units=imperial`
+// ========== Grab the weather data and W A I T
+fetch(myWeather)
+.then((response) => response.json())
+.then((allData) => {
+    currentWeather(allData)
+}) // end waiting
+
+function currentWeather(weatherResults) {
+  console.log(weatherResults)
+  const myTemperature = document.querySelector('#temp')
+  myTemperature.textContent = weatherResults.main.temp
+  const myDescription = document.querySelector('#desc')
+  myDescription.textContent = weatherResults.weather[0].description
+  const myIcon = document.querySelector('#icon')
+  myIcon.src=`https://openweathermap.org/img/w/${weatherResults.weather[0].icon}.png`
+  myIcon.alt=weatherResults.weather[0].description
+}
